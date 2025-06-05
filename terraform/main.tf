71f6fcafa001b27fcd953ee1b57356561894e02a
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-05 22:32:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 
 resource "aws_ecs_cluster" "this" {
-  name = "mycluster"
+  name = "app-cluster"
 }
 
 
@@ -12,7 +12,7 @@ module "compute" {
   source = "../tf-modules/compute"
 
 
-  ecs_cluster_id    = arn:aws:ecs:us-east-1:151182331903:cluster/mycluster
+  ecs_cluster_id    = arn:aws:ecs:us-east-1:151182331903:cluster/app-cluster
   ecr_repo_name     = myecr
   ecs_cluster_name  = "mycluster"
    ec2_ami           = ami-0731becbf832f281e
