69d7574025343f4c9f462529817630ace935f365
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-06 11:42:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,12 +14,12 @@ module "compute" {
 
   ecs_cluster_id    = "arn:aws:ecs:us-east-1:151182331903:cluster/app-cluster"
   ecr_repo_name     = "myecr"
-  ecs_cluster_name  = "mycluster"
+  ecs_cluster_name  = "app-cluster"
    ec2_ami           = "ami-0731becbf832f281e"
   ec2_instance_type = "t2.micro"
-  subnet_id         = "subnet-0fec3e10ad8729474" 
+  subnet_id         = "subnet-0cdd720102c04a689" 
   key_name          = "anu.pem"
-  vpc_id            = "vpc-0f1299c754e4b41a3"
+  vpc_id            = "vpc-08ca0bc18af0f4dd3"
   public_subnets  = ["10.0.0.0/28", "10.0.0.64/28"]
 }
 
