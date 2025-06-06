3e79c9d00cd4be8444ed330725ac53c44a54d242
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-06 12:58:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,4 +10,10 @@ resource "aws_ecs_cluster" "this" {
 
 module "compute" {
   source = "../tf-modules/compute"
+
+ ecs_cluster_id  = aws_ecs_cluster.main.id         # or your cluster ID
+  key_name        = "anu"                           # or from a variable
+  subnet_id       = aws_subnet.private_subnet.id           # or from a list
+  vpc_id          = aws_vpc.main.id                 # or from data source
+  public_subnets  = [aws_subnet.public_subnet, aws_subnet.private_subnet]
 }
