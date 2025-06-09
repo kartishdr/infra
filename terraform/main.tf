07b23327173e4d56d85696b5d380cb0db04867ef
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-09 10:54:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-resource "aws_ecs_cluster" "this" {
-  name = "app-cluster"
+# Discover the VPC (e.g. the default, or use tag filters)
+data "aws_vpc" "existing" {
+  default = true
 }
 
+# Discover public subnets in that VPC
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+}
 
 module "compute" {
-  source = "../tf-modules/compute"
+  source            = "../../../tf-modules/compute"
 
- ecs_cluster_id  = aws_ecs_cluster.main.id         # or your cluster ID
-  key_name        = "anu"                           # or from a variable
-  subnet_id       = aws_subnet.private_subnet.id           # or from a list
-  vpc_id          = aws_vpc.main.id                 # or from data source
-  public_subnets  = [aws_subnet.public_subnet, aws_subnet.private_subnet]
+  vpc_id            = data.aws_vpc.existing.id
+  public_subnets    = data.aws_subnets.public.ids
+
+  ec2_instance_type = "t2.micro"
+  ecr_repo_name     = "my-ecr-repo"
+  ecs_cluster_name  = "my-ecs-cluster"
+  alb_name          = "my-alb"
 }
+
