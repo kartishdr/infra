1c39a5e490c1fe574eeba5000ab1db863c4af8f2
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-09 12:41:40 +0530

    Update outputs.tf

--- tf-modules/compute/outputs.tf
+++ tf-modules/compute/outputs.tf
@@ -1,15 +1,3 @@
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app_repo.repository_url
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
-}
-
-output "ec2_instance_public_ip" {
-  value = aws_instance.bastion.public_ip
-}
-
 output "alb_dns_name" {
-  value = aws_lb.app_alb.dns_name
+  value = aws_lb.this.dns_name
 }
