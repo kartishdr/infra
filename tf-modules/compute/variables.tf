3aaf9f87b6a8ec752858e332669acdb9d9901847
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-09 12:41:04 +0530

    Update variables.tf

--- tf-modules/compute/variables.tf
+++ tf-modules/compute/variables.tf
@@ -1,44 +1,23 @@
+variable "ec2_instance_type" {
+  type = string
+}
+
 variable "ecr_repo_name" {
-  default = "my-app-repo"
+  type = string
 }
 
 variable "ecs_cluster_name" {
-  type        = string
-  default     = "simple-ecs-cluster"
-  description = "Name of the ECS cluster"
+  type = string
 }
 
-
-
-variable "ecs_cluster_id" {
-  description = "ID of the ECS cluster"
-  type        = string
-}
-
-
-variable "ec2_ami" {
-  description = "AMI ID for EC2 instance"
-  default     = "ami-084568db4383264d4" # example for Amazon Linux 2
-}
-
-variable "ec2_instance_type" {
-  default = "t2.micro"
-}
-
-variable "subnet_id" {
-  description = "Subnet for EC2 instance"
-}
-
-variable "key_name" {
-  description = "EC2 key pair name"
+variable "alb_name" {
+  type = string
 }
 
 variable "vpc_id" {
-  description = "VPC ID for your resources"
+  type = string
 }
 
 variable "public_subnets" {
-  description = "List of public subnets for the ALB"
-  type        = list(string)
+  type = list(string)
 }
-                          
