e0edfc3925c97a0a0a187fea894ca22b7a2f7487
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-12 11:10:19 +0530

    Create outputs.tf

---
+++ tf-modules/network/outputs.tf
@@ -0,0 +1,28 @@
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+  description = "The ID of the VPC"
+}
+
+output "subnet_id" {
+  value = aws_subnet.public_subnet.id
+  description = "The ID of the subnet"
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.gw.id
+  description = "The ID of the internet gateway"
+}
+
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
