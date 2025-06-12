385d1be866896a06ff8d9856b45ecc1d1603c8ce
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-12 22:05:27 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,31 @@
 module "network" {
   source              = "../tf-modules/network-module"
-  ...
+}
+terraform {
+  backend "s3" {
+    bucket         = "dev-kpro-ue1-account-no-tf-state"               # S3 bucket name
+    key            = "env/us-east-1/network/terraform.tfstate"        # Path inside the bucket
+    region         = "us-east-1"                                      # Region of S3 bucket
+ #   dynamodb_table = "terraform-locks"                                # Optional: for state locking
+    encrypt        = true                                             # Encrypt state at rest
+  }
 }
 
 module "storage" {
   source              = "../tf-modules/storage-module"
   depends_on          = [module.network]
-  ...
+   vpc_id               = "vpc-065e7588e40c7c2b3"
+ security_group_id     = "sg-06bc8415c084c4a56"
+db_subnet_group_name  = "my-db-subnet-group"
 }
 
 module "compute" {
   source              = "../tf-modules/compute-module"
   depends_on          = [module.storage]
-  ...
+  vpc_id            = data.aws_vpc.existing.id
+  public_subnets    = data.aws_subnets.public.ids
+  ec2_instance_type = "t2.micro"
+  ecr_repo_name     = "my-ecr-repo"
+  ecs_cluster_name  = "my-ecs-cluster"
+  alb_name          = "my-alb"
 }
