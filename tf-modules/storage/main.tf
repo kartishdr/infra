0abcea6dc16f80798a78ac2bb132e7db59c4767f
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-13 22:42:10 +0530

    Update main.tf

--- tf-modules/storage/main.tf
+++ tf-modules/storage/main.tf
@@ -61,3 +61,45 @@ resource "aws_db_subnet_group" "my_db_subnet_group" {
     Name = "My DB Subnet Group"
   }
 }
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  description = "Allow RDS access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 3306            # or 5432 for PostgreSQL
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   #
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+  Name = "rds-security-group"
+  }
+}
+
+
+
+
+
+resource "aws_db_instance" "mydb" {
+  allocated_storage    = 20
+  engine               = "mysql"             # or "postgres"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+   username             = var.db_user
+  password             = var.db_password
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  publicly_accessible  = false
+  db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
+                      
