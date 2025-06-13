917bb4e2ba9c3b5548af9df567ef71c7a580addb
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-13 22:31:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,19 +5,41 @@ provider "aws" {
 module "network" {
   source              = "../tf-modules/network"
 }
+
+data "aws_vpc" "existing" {
+  default = true
+}
+
+data "aws_subnets" "in_vpc" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+}
 module "storage" {
-  source              = "../tf-modules/storage"
-  depends_on          = [module.network]
-   vpc_id               = "vpc-065e7588e40c7c2b3"
- security_group_id     = "sg-06bc8415c084c4a56"
+  source              = "../../../tf-modules/storage"
+  vpc_id              = data.aws_vpc.existing.id
+ security_group_id    = "sg-06bc8415c084c4a56"
 db_subnet_group_name  = "my-db-subnet-group"
+subnet_ids = data.aws_subnets.in_vpc.ids
+}
+data "aws_vpc" "existing" {
+  default = true
+}
+# Discover public subnets in that VPC
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
 }
 
 module "compute" {
-  source              = "../tf-modules/compute"
-  depends_on          = [module.storage]
+  source            = "../../../tf-modules/compute"
+
   vpc_id            = data.aws_vpc.existing.id
   public_subnets    = data.aws_subnets.public.ids
+
   ec2_instance_type = "t2.micro"
   ecr_repo_name     = "my-ecr-repo"
   ecs_cluster_name  = "my-ecs-cluster"
