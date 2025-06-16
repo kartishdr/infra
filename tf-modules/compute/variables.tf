be975eda024c34d85483631f2a80cebca8ad99c9
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-16 13:40:49 +0530

    Update variables.tf

--- tf-modules/compute/variables.tf
+++ tf-modules/compute/variables.tf
@@ -1,19 +1,1 @@
-variable "ec2_instance_type" {
-  type = string
-}
 
-variable "ecr_repo_name" {
-  type = string
-}
-
-variable "ecs_cluster_name" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "public_subnets" {
-  type = list(string)
-}
