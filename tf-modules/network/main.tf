c7a01aa8d3948fac0003751821e0b2c8c06f0359
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-06-24 11:56:10 +0530

    Update main.tf

--- tf-modules/network/main.tf
+++ tf-modules/network/main.tf
@@ -42,7 +42,7 @@ resource "aws_internet_gateway" "gw" {
 
 # Create a NAT Gateway in the public subnet
 resource "aws_eip" "nat_eip" {
-  vpc = true
+  domain = "vpc"
 }
 
 resource "aws_nat_gateway" "nat" {
